a68178917d889ae5635894cc7092ee8476058c43
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-03 12:20:55 +0100

    Add non-python third party packages provenance

--- core/provenance.py
+++ core/provenance.py
@@ -1,14 +1,17 @@
 import os
+import subprocess
 
 from experiments import provenance
-
-pa_ownpckgs = ['experiments', 'clusterjobs', 'scicfg',
-               'learners', 'fastlearners', 'explorers', 'environments']
-pa_thirdparty = ['scipy', 'numpy', 'sklearn', 'shapely']
 
 GIT_DIR = os.path.abspath(os.path.join(__file__, '../..'))
 code_commit = provenance.git_commit(GIT_DIR)
 code_dirty  = provenance.git_dirty(GIT_DIR)
+
+
+pa_ownpckgs = ['experiments', 'clusterjobs', 'scicfg',
+               'learners', 'fastlearners', 'explorers', 'environments']
+pa_thirdparty = ['scipy', 'numpy', 'sklearn', 'shapely']
+pa_nonpython = [('geos', 'geos-config --version')]
 
 def planar_arms_data():
     plat = provenance.platform_info()
@@ -21,17 +24,27 @@ def planar_arms_data():
         owns.append((name, cfg.commit))
     owns.sort()
 
-    # third party packages
-    tp_cfgs = provenance.packages_info(pa_thirdparty)
+    # third party python packages
+    pytp_cfgs = provenance.packages_info(pa_thirdparty)
+    pytps = []
+    for name, cfg in pytp_cfgs._branches:
+        pytps.append((name, cfg.version))
+    pytps.sort()
+
+    # third party python packages
     tps = []
-    for name, cfg in tp_cfgs._branches:
-        tps.append((name, cfg.version))
+    for name, version_cmd in pa_nonpython:
+        p = subprocess.Popen(version_cmd.split(), stdout=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        stdout = stdout.decode('utf8').replace('\n', '')
+        tps.append((name, stdout))
     tps.sort()
 
-    return plat, owns, tps
+
+    return plat, owns, pytps, tps
 
 def planar_arms():
-    plat, owns, tps = planar_arms_data()
+    plat, owns, pytps, tps = planar_arms_data()
 
     if not code_dirty:
         print('All the code involved in the computation is commited (no git repository in dirty state).')
@@ -46,12 +59,18 @@ def planar_arms():
         print('    {}{} [{}]'.format(name, ' '*(max_name - len(name)), commit))
     print('')
 
-    print('Installed third-party packages:')
+    print('Installed third-party python packages:')
+    for name, version in pytps:
+        print('    {} {}'.format(name, version))
+    print('')
+
+    print('Installed third-party non-python packages:')
     for name, version in tps:
         print('    {} {}'.format(name, version))
     print('')
 
-    print('{} {} {}'.format(plat.python.implementation,
-                            plat.python.version, plat.python.build))
-    print('[{}]'.format(plat.python.compiler))
-    print('on {}'.format(plat.uname[3]))
+    print('Executed with:')
+    print('    {} {} {}'.format(plat.python.implementation,
+                                plat.python.version, plat.python.build))
+    print('    [{}]'.format(plat.python.compiler))
+    print('    on {}'.format(plat.uname[3]))
