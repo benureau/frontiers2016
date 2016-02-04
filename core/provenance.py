e7587f09a08b7b808704c061f9529230303700f5
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-04 02:00:39 +0100

    Better provenance code

--- core/provenance.py
+++ core/provenance.py
@@ -1,76 +1,130 @@
 import os
+import time
 import subprocess
 
 from experiments import provenance
 
+
 GIT_DIR = os.path.abspath(os.path.join(__file__, '../..'))
-code_commit = provenance.git_commit(GIT_DIR)
-code_dirty  = provenance.git_dirty(GIT_DIR)
+
+# planar arms
+PA_RESEARCH_PKGS = ['experiments', 'clusterjobs', 'scicfg',
+                    'learners', 'fastlearners', 'explorers', 'environments']
+PA_THIRDPARTY_PY = ['scipy', 'numpy', 'sklearn', 'shapely']
+PA_THIRDPARTY    = [('geos', 'geos-config --version')]
+
+# object interaction
+OI_RESEARCH_PKGS = PA_RESEARCH_PKGS + ['fastlearners', 'dovecot']
+OI_THIRDPARTY_PY = PA_THIRDPARTY_PY + ['sympy']
+OI_THIRDPARTY    = (PA_THIRDPARTY +
+                    [('Eigen',  'pkg-config eigen3 --modversion'),
+                     ('libccd', 'pkg-config ccd    --modversion'),
+                     ('FCL',    'pkg-config fcl    --modversion'),
+                     ('FLANN',  'pkg-config flann  --modversion')])
+OI_MANUAL        = [('dmpbbo', '[4bbb90ae679053e04bb4604bee0acbaf75f64875]'),
+                    ('V-REP', '3.2.3, rev 4'),
+                    ('Boost', '1.55')]
 
 
-pa_ownpckgs = ['experiments', 'clusterjobs', 'scicfg',
-               'learners', 'fastlearners', 'explorers', 'environments']
-pa_thirdparty = ['scipy', 'numpy', 'sklearn', 'shapely']
-pa_nonpython = [('geos', 'geos-config --version')]
+class ProvenanceData(object):
+    """An object that tracks provenance data"""
 
-def planar_arms_data():
-    plat = provenance.platform_info()
+    def __init__(self, code_dir, research_pkgs=(),
+                 thirdparty_py=(), thirdparty=(), manual=()):
+        self.timestamp = time.time()
+        self.dirty = False # has any of research_pkgs or code_dir a dirty repo?
+        self.plat_info = provenance.platform_info()
 
-    # own packages
-    own_cfgs = provenance.packages_info(pa_ownpckgs)
-    owns = []
-    for name, cfg in own_cfgs._branches:
-        assert not cfg.dirty
-        owns.append((name, cfg.commit))
-    owns.sort()
+        self._code          = None
+        self._research_pkgs = []
+        self._thirdparty_py = []
+        self._thirdparty    = []
 
-    # third party python packages
-    pytp_cfgs = provenance.packages_info(pa_thirdparty)
-    pytps = []
-    for name, cfg in pytp_cfgs._branches:
-        pytps.append((name, cfg.version))
-    pytps.sort()
+        self.register_code(code_dir)
+        for pkg_name in research_pkgs:
+            self.register_package(pkg_name, research=True)
+        for pkg_name in thirdparty_py:
+            self.register_package(pkg_name, research=False)
+        for pkg_name, version_cmd in thirdparty:
+            self.register_thirdparty(pkg_name, version_cmd)
+        for pkg_name, version in manual:
+            self.register_manual(pkg_name, version)
 
-    # third party python packages
-    tps = []
-    for name, version_cmd in pa_nonpython:
+    def register_code(self, code_dir):
+        assert os.path.isdir(code_dir)
+        commit = provenance.git_commit(code_dir)
+        dirty  = provenance.git_dirty(code_dir)
+        self.dirty = self.dirty or dirty
+        self._code = (code_dir, commit, dirty)
+
+    def register_package(self, name, research=False):
+        pkg_info = provenance.packages_info([name])
+        for name, info in pkg_info._branches:
+            self.dirty = self.dirty or info.dirty
+            if research:
+                self._research_pkgs.append((name, info.commit, info.dirty))
+                self._research_pkgs.sort()
+            else:
+                self._thirdparty_py.append((name, info.version))
+                self._thirdparty_py.sort()
+
+    def register_thirdparty(self, name, version_cmd):
         p = subprocess.Popen(version_cmd.split(), stdout=subprocess.PIPE)
         stdout, stderr = p.communicate()
-        stdout = stdout.decode('utf8').replace('\n', '')
-        tps.append((name, stdout))
-    tps.sort()
+        version = stdout.decode('utf8').replace('\n', '')
+        self._thirdparty.append((name, version))
+        self._thirdparty.sort()
 
+    def register_manual(self, name, version):
+        self._thirdparty.append((name, version))
+        self._thirdparty.sort()
 
-    return plat, owns, pytps, tps
+    def message(self):
+        s = ''
+
+        # code text
+        if not self.dirty:
+            s += ('All the code involved in the computation is commited '
+                  '(no git repository in dirty state).\n')
+        else:
+            s += ('Some code involved in the computation has not been commited '
+                  '(some git repository in dirty state).\n')
+        s += 'This code was executed with commit {}.\n\n'.format(self._code[1])
+
+        # research package text
+        s += 'Installed research packages (available in the submodules/ directory):\n'
+        max_name = max(len(name) for name, commit, dirty, in self._research_pkgs)
+        for name, commit, dirty, in self._research_pkgs:
+            s += '    {}{} [{}]\n'.format(name, ' '*(max_name - len(name)), commit)
+        s += '\n'
+
+        # third-party python packages
+        s += 'Installed third-party python packages:\n'
+        for name, version in self._thirdparty_py:
+            s += '    {} {}\n'.format(name, version)
+        s += '\n'
+
+        # non-python third-party packages
+        s += 'Installed third-party non-python packages:\n'
+        for name, version in self._thirdparty:
+            s += '    {} {}\n'.format(name, version)
+        s += '\n'
+
+        # platform info
+        s += 'Executed with:\n'
+        s += '    {} {} {}\n'.format(self.plat_info.python.implementation,
+                                     self.plat_info.python.version,
+                                     self.plat_info.python.build)
+        s += '    [{}]\n'.format(self.plat_info.python.compiler)
+        s += '    on {}\n'.format(self.plat_info.uname[3])
+
+        return s
+
 
 def planar_arms():
-    plat, owns, pytps, tps = planar_arms_data()
+    return ProvenanceData(GIT_DIR, PA_RESEARCH_PKGS,
+                          PA_THIRDPARTY_PY, PA_THIRDPARTY)
 
-    if not code_dirty:
-        print('All the code involved in the computation is commited (no git repository in dirty state).')
-    else:
-        print('Some code involved in the computation has not been commited (some git repository in dirty state).')
-    print('This code was executed with commit {}.'.format(code_commit))
-    print('')
-
-    print('Installed research packages (available in the submodules/ directory):')
-    max_name = max(len(name) for name, commit in owns)
-    for name, commit in owns:
-        print('    {}{} [{}]'.format(name, ' '*(max_name - len(name)), commit))
-    print('')
-
-    print('Installed third-party python packages:')
-    for name, version in pytps:
-        print('    {} {}'.format(name, version))
-    print('')
-
-    print('Installed third-party non-python packages:')
-    for name, version in tps:
-        print('    {} {}'.format(name, version))
-    print('')
-
-    print('Executed with:')
-    print('    {} {} {}'.format(plat.python.implementation,
-                                plat.python.version, plat.python.build))
-    print('    [{}]'.format(plat.python.compiler))
-    print('    on {}'.format(plat.uname[3]))
+def object_interaction():
+    return ProvenanceData(GIT_DIR, OI_RESEARCH_PKGS,
+                          OI_THIRDPARTY_PY, OI_THIRDPARTY, OI_MANUAL)
