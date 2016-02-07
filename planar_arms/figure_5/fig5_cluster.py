b91360803dc364461dcd640d9fb08827e416d9f9
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-07 14:49:36 +0100

    Adjust fig5_cluster.planar() path

--- planar_arms/figure_5/fig5_cluster.py
+++ planar_arms/figure_5/fig5_cluster.py
@@ -10,7 +10,7 @@ import exp_cfgs
 from exp_factory import make_exp
 
 
-def planar(path='frontiers/planar_arms', rep=100):
+def planar(path='frontiers2016/planar_arms', rep=100):
     cfg = exp_cfgs.planar_cfg._deepcopy()
     nor_diff = {'exp.path'         : path,
                 'exp.prefix'       : ('kin_reuse',),
@@ -21,13 +21,11 @@ def planar(path='frontiers/planar_arms', rep=100):
     tgt_diff = nor_diff
 
     return make_exp([('kin20_150', 'kin20_150_0.9')],
-                     cfg=exp_cfgs.planar_cfg._deepcopy(),
-                     nor_diff=nor_diff, src_diff=src_diff, tgt_diff=tgt_diff,
-                     #src_ex_names=('rmb50.rgb.p0.05',),
-                     src_ex_names=('random.goal_50',),
-                     #nor_ex_names=('rmb50.rgb.p0.05',),
-                     nor_ex_names=('random.goal_50',),
-                     tgt_ex_names=('reuse_50_1.0_20_p0.05',))
+                    cfg=exp_cfgs.planar_cfg._deepcopy(),
+                    nor_diff=nor_diff, src_diff=src_diff, tgt_diff=tgt_diff,
+                    src_ex_names=('rmb50.rgb.p0.05',),
+                    nor_ex_names=('rmb50.rgb.p0.05',),
+                    tgt_ex_names=('reuse_50_1.0_20_p0.05',))
 
 
 if __name__ == '__main__':
