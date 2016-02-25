3209cfe8720caf1c06f4b6d8b798173730ea0121
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-25 12:38:49 +0100

    Data dir as environment variable

--- core/paths.py
+++ core/paths.py
@@ -7,24 +7,22 @@ import scicfg
 # You should modify this file to your folder/architecture layout
 
 cfg = scicfg.SciConfig()
-cfg._describe('meta.user', instanceof=str, docstring='the name of the user (used on clusters by qstat)')
-cfg._describe('meta.rootpath', instanceof=str, docstring='the path towards the data')
+cfg._describe('meta.user', instanceof=str,
+              docstring='the name of the user (used on clusters by qstat)')
+cfg._describe('meta.rootpath', instanceof=str,
+              docstring='the path towards the data')
 
-uname = os.uname()
-if uname[0] == 'Linux':
-    cfg.meta.user     = 'fbenurea'
-    cfg.meta.rootpath = '/scratch/fbenurea/'
-    testset_dir       = '/home/fbenurea/code/testsets/'
-
-elif uname[0] == 'Darwin':
-    cfg.meta.user     = 'fabien'
-    cfg.meta.rootpath = '~/research/data/'
-    testset_dir       = '/Users/fabien/research/enc/projects/phd/code/testsets/'
-
+if 'BENUREAU_FRONTIERS2016_DATA' in os.environ:
+    cfg.meta.rootpath = os.environ['BENUREAU_FRONTIERS2016_DATA']
+    if 'BENUREAU_FRONTIERS2016_USER' in os.environ:
+        cfg.meta.user = os.environ['BENUREAU_FRONTIERS2016_USER']
 else:
-    raise ValueError('Platform not known, please setup user and rootpath.')
-
-testset_filepaths  = {'kin_one': testset_dir + 'testset_kinone',
-                      'dmp2d'  : testset_dir + 'testset_dmp2d',
-                      'dov_a6' : testset_dir + 'testset_dov_a6',
-                     }
+    uname = os.uname()
+    if uname[0] == 'Linux':
+        cfg.meta.user     = 'fbenurea'
+        cfg.meta.rootpath = '/scratch/fbenurea/'
+    elif uname[0] == 'Darwin':
+        cfg.meta.user     = 'fabien'
+        cfg.meta.rootpath = '~/research/data/'
+    else:
+        raise ValueError('Platform not known, please setup user and rootpath.')
